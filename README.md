5c6d9f162dec20480dde7f9562114521538db5f9
Langdon Himebaugh <lang@himebaugh.com>
2018-04-11 11:19:24 -0400

    Change SearchableActivity from AsyncTask to CursorLoader.

---
+++ README.md
@@ -0,0 +1,18 @@
+# EmployeeDirectory
+Building a Mobile Employee Directory on Android.
+
+Step 1: Display a ListActivity with some data.<br />
+Step 2: Load data into the ListActivity from an XML file via XmlParser.<br />
+Step 3: Save (Persist) the data into a SQLite Database & Load a ListView from a SQLite Database.<br />
+Step 4: Pass data to DetailActivity to display more data and provide other functionality (w/ intent.putExtra).<br />
+Step 5: Create a ContentProvider to access the database.<br />
+Step 6: Create a Search Interface using Android’s search dialog.<br />
+Step 7: Create an App Widget with it’s own ContentProvider & Service.<br />
+
+https://github.com/langhimebaugh/EmployeeDirectory/tree/Step-1
+https://github.com/langhimebaugh/EmployeeDirectory/tree/Step-2
+https://github.com/langhimebaugh/EmployeeDirectory/tree/Step-3
+https://github.com/langhimebaugh/EmployeeDirectory/tree/Step-4
+https://github.com/langhimebaugh/EmployeeDirectory/tree/Step-5
+https://github.com/langhimebaugh/EmployeeDirectory/tree/Step-6
+https://github.com/langhimebaugh/EmployeeDirectory/tree/Step-7
--- app/src/main/java/com/himebaugh/employeedirectory/SearchableActivity.java
+++ app/src/main/java/com/himebaugh/employeedirectory/SearchableActivity.java
@@ -4,8 +4,13 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -13,11 +18,15 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class SearchableActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class SearchableActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, AdapterView.OnItemClickListener {
 
     private static final String TAG = SearchableActivity.class.getSimpleName();
 
-    private ListView listView;
+    private static final int LOADER_ID = 2;
+
+    private ListView mListView;
+
+    private SimpleCursorAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +36,38 @@ public class SearchableActivity extends AppCompatActivity implements AdapterView
 
         setContentView(R.layout.activity_searchable);
 
-        listView = findViewById(R.id.search_list);
+        mListView = findViewById(R.id.search_list);
 
-        listView.setOnItemClickListener(this);
+        mListView.setOnItemClickListener(this);
+
+        String[] dataColumns = {
+                EmployeeDatabase.COLUMN_ID,
+                EmployeeDatabase.COLUMN_FIRSTNAME,
+                EmployeeDatabase.COLUMN_TITLE,
+                EmployeeDatabase.COLUMN_DEPARTMENT,
+                EmployeeDatabase.COLUMN_CITY,
+                EmployeeDatabase.COLUMN_OFFICE_PHONE,
+                EmployeeDatabase.COLUMN_MOBILE_PHONE,
+                EmployeeDatabase.COLUMN_EMAIL,
+                EmployeeDatabase.COLUMN_PICTURE
+        };
+        int[] viewIDs = {
+                R.id.list_item_emp_id,
+                R.id.list_item_name,
+                R.id.list_item_title,
+                R.id.list_item_department,
+                R.id.list_item_city,
+                R.id.list_item_office_phone,
+                R.id.list_item_mobile_phone,
+                R.id.list_item_email,
+                R.id.list_item_picture
+        };
+
+        // Create an empty adapter for the ListView.
+        // The search results will be put in when they are fully loaded.
+        mAdapter = new android.support.v4.widget.SimpleCursorAdapter(getBaseContext(), R.layout.list_item, null, dataColumns, viewIDs, 0);
+
+        mListView.setAdapter(mAdapter);
 
         handleIntent(getIntent());
     }
@@ -71,81 +109,52 @@ public class SearchableActivity extends AppCompatActivity implements AdapterView
 
     private void doSearch(String query) {
 
-        new SearchTask().execute(query);
+        Bundle data = new Bundle();
+        data.putString("query", query);
+
+        // Initialize the search results loader.
+        getSupportLoaderManager().initLoader(LOADER_ID, data, this);
 
     }
 
-    private class SearchTask extends AsyncTask<String, Void, Cursor> {
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle data) {
 
-        @Override
-        protected Cursor doInBackground(String... params) {
+        String wildcardQuery = "%" + data.getString("query") + "%";
 
-            String wildcardQuery = "%" + params[0] + "%";
+        Uri uri = EmployeeProvider.CONTENT_URI;
+        String[] projection = {
+                EmployeeDatabase.COLUMN_ID,
+                EmployeeDatabase.COLUMN_FIRSTNAME,
+                EmployeeDatabase.COLUMN_LASTNAME,
+                EmployeeDatabase.COLUMN_TITLE,
+                EmployeeDatabase.COLUMN_DEPARTMENT,
+                EmployeeDatabase.COLUMN_CITY,
+                EmployeeDatabase.COLUMN_OFFICE_PHONE,
+                EmployeeDatabase.COLUMN_MOBILE_PHONE,
+                EmployeeDatabase.COLUMN_EMAIL,
+                EmployeeDatabase.COLUMN_PICTURE
+        };
+        String selection = EmployeeDatabase.COLUMN_FIRSTNAME + " LIKE ? OR " + EmployeeDatabase.COLUMN_LASTNAME + " LIKE ?";
+        String[] selectionArgs = {wildcardQuery, wildcardQuery};
+        String sortOrder = EmployeeDatabase.COLUMN_LASTNAME + " COLLATE LOCALIZED ASC";
 
-            Uri uri = EmployeeProvider.CONTENT_URI;
-            String[] projection = {
-                    EmployeeDatabase.COLUMN_ID,
-                    EmployeeDatabase.COLUMN_FIRSTNAME,
-                    EmployeeDatabase.COLUMN_LASTNAME,
-                    EmployeeDatabase.COLUMN_TITLE,
-                    EmployeeDatabase.COLUMN_DEPARTMENT,
-                    EmployeeDatabase.COLUMN_CITY,
-                    EmployeeDatabase.COLUMN_OFFICE_PHONE,
-                    EmployeeDatabase.COLUMN_MOBILE_PHONE,
-                    EmployeeDatabase.COLUMN_EMAIL,
-                    EmployeeDatabase.COLUMN_PICTURE
-            };
-            String selection = EmployeeDatabase.COLUMN_FIRSTNAME + " LIKE ? OR " + EmployeeDatabase.COLUMN_LASTNAME + " LIKE ?";
-            String[] selectionArgs = {wildcardQuery, wildcardQuery};
-            String sortOrder = EmployeeDatabase.COLUMN_LASTNAME + " COLLATE LOCALIZED ASC";
+        CursorLoader cursorLoader = new CursorLoader(this, uri, projection, selection, selectionArgs, sortOrder);
 
-            Cursor cursor = getContentResolver().query(uri, projection, selection, selectionArgs, sortOrder);
-
-            return cursor;
-        }
-
-        @Override
-        protected void onPostExecute(Cursor cursor) {
-
-            String[] dataColumns = {
-                    EmployeeDatabase.COLUMN_ID,
-                    EmployeeDatabase.COLUMN_FIRSTNAME,
-                    EmployeeDatabase.COLUMN_TITLE,
-                    EmployeeDatabase.COLUMN_DEPARTMENT,
-                    EmployeeDatabase.COLUMN_CITY,
-                    EmployeeDatabase.COLUMN_OFFICE_PHONE,
-                    EmployeeDatabase.COLUMN_MOBILE_PHONE,
-                    EmployeeDatabase.COLUMN_EMAIL,
-                    EmployeeDatabase.COLUMN_PICTURE
-            };
-            int[] viewIDs = {
-                    R.id.list_item_emp_id,
-                    R.id.list_item_name,
-                    R.id.list_item_title,
-                    R.id.list_item_department,
-                    R.id.list_item_city,
-                    R.id.list_item_office_phone,
-                    R.id.list_item_mobile_phone,
-                    R.id.list_item_email,
-                    R.id.list_item_picture
-            };
-
-            android.support.v4.widget.SimpleCursorAdapter adapter = new android.support.v4.widget.SimpleCursorAdapter(getBaseContext(), R.layout.list_item, cursor, dataColumns, viewIDs, 0);
-
-            Log.i(TAG, "onPostExecute: " + cursor.getCount());
-
-            if (cursor.getCount() == 0) {
-                // no results found
-            }
-
-            if (adapter != null && listView != null) {
-                listView.setAdapter(adapter);
-            }
-
-        }
-
+        return cursorLoader;
     }
 
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        // Put the results in the adapter.
+        mAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -184,4 +193,14 @@ public class SearchableActivity extends AppCompatActivity implements AdapterView
         finish();
 
     }
+
+//    /**
+//     * This method is called after this activity has been paused or restarted.
+//     */
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        getSupportLoaderManager().restartLoader(LOADER_ID, null, this);
+//    }
 }
